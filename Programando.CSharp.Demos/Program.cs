09b8999453788ec42daf49529a17aa9aedcf9e82
Francisco de Borja Cabeza Rozas <borjacabeza@msn.com>
2023-03-28 12:25:02 +0200

    Commit Demo

--- Programando.CSharp.Demos/Program.cs
+++ Programando.CSharp.Demos/Program.cs
@@ -3,8 +3,8 @@
     public class Program
     {
         public static void Main(string[] args)
-        {            
-
+        {
+            Console.WriteLine("Hola Mundo !!!");
         }
     }
 }
