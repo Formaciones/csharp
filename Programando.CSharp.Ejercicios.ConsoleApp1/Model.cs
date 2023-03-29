9c9691373584cf0d092c2d7c4a7c3af3425e736a
Francisco de Borja Cabeza Rozas <borjacabeza@msn.com>
2023-03-29 10:40:15 +0200

    Commit incorporación del DataList

---
+++ Programando.CSharp.Ejercicios.ConsoleApp1/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programando.CSharp.Ejercicios.ConsoleApp1
+{
+	internal class Model
+	{
+	}
+}
---
+++ Programando.CSharp.Ejercicios.ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programando.CSharp.Ejercicios.ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
