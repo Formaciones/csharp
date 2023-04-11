d44330b0d3527e51e25d73f3980ea63c8afe0adb
Francisco de Borja Cabeza Rozas <borjacabeza@msn.com>
2023-04-11 13:29:53 +0200

    Commit Sessión 11-Abr

--- Programando.CSharp.WebApplication1/Controllers/EmpleadosController.cs
+++ Programando.CSharp.WebApplication1/Controllers/EmpleadosController.cs
@@ -1,25 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Programando.CSharp.WebApplication1.Model;
 
 namespace Programando.CSharp.WebApplication1.Controllers
 {
-    public class ClientesController : Controller
+    public class EmpleadosController : Controller
     {
         private readonly ModelNorthwind _context;
 
-        public ClientesController(ModelNorthwind context)
-        {
+        public EmpleadosController(ModelNorthwind context)
+        { 
             _context = context;
         }
 
         public IActionResult Index()
         {
-            var clientes = _context.Customers
-                .OrderBy(r => r.CompanyName)
-                .ToList();
+            var empleados = _context.Employees.ToList();
 
-            return View(clientes);
+            return View(empleados);
         }
     }
 }
